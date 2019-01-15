3f48134706b4e63c5c6caad84550b716d1583e85
imiaoo <46514981+imiaoo@users.noreply.github.com>
2019-01-16 07:56:54 +0800

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ use App\Services\Boot;
 
 //  BASE_PATH
 define('BASE_PATH', __DIR__);
-define('VERSION', '20180419');
+define('VERSION', '20170829');
 
 // Vendor Autoload
 require BASE_PATH.'/vendor/autoload.php';
@@ -20,6 +20,7 @@ require BASE_PATH."/config/.config.php";
 
 
 
+Boot::loadEnv();
 Boot::setDebug();
 //Boot::setVersion(VERSION);
 // config time zone
